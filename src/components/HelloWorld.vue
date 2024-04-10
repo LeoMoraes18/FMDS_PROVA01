ccf4cc8aa81bbf4e9775b29ed10734c54d24183b
Leonardo <djleomoraes@outlook.com>
2024-04-09 23:51:14 -0400

    primeiro commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,27 @@
+<template>
+  <v-container class="fill-height">
+    <v-responsive
+      class="align-centerfill-height mx-auto"
+      max-width="900"
+    >
+      <v-row>
+        <v-col cols="12">
+          <v-card>
+            <v-card-title>
+              <h1>Hello World</h1>
+            </v-card-title>
+            <v-card-text>
+              <p>
+                This is a simple example of a VitePress site with Vuetify.
+              </p>
+            </v-card-text>
+          </v-card>
+        </v-col>
+      </v-row>
+    </v-responsive>
+  </v-container>
+</template>
+
+<script setup>
+  //
+</script>
---
+++ src/layouts/default.vue
@@ -0,0 +1,39 @@
+<template>
+  <v-app id="inspire">
+    <v-app-bar
+      class="px-3"
+      density="compact"
+      flat
+    >
+      <v-spacer></v-spacer>
+
+      <v-tabs
+        color="grey-darken-2"
+        centered
+      >
+        <v-tab
+          v-for="link in links"
+          :key="link"
+          :text="link"
+        ></v-tab>
+      </v-tabs>
+      <v-spacer></v-spacer>
+    </v-app-bar>
+
+    <v-main class="bg-grey-lighten-3">
+      <v-container>
+        <RouterView />
+      </v-container>
+    </v-main>
+    <AppFooter />
+  </v-app>
+</template>
+<script setup>
+import AppFooter from '@/components/AppFooter.vue'
+  const links = [
+    'Dashboard',
+    'Messages',
+    'Profile',
+    'Updates',
+  ]
+</script>
